0fa675a17987ac1ab06f473800a26d46be80cbf4
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2017-06-18 20:51:12 -0400

    Started on postlogin routes and authentication.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,32 @@ import { LoginFormComponent } from './components/login-form/login-form.component
 //Pages
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PostloginComponent } from './pages/postlogin/postlogin.component';
+import { SwitchComponent } from './pages/switch/switch.component';
+import { MapComponent } from './pages/map/map.component';
 // Services
 import { UserService} from './services/user/user.service';
 // Directives
+
+// Guards
+import { AuthGuard } from './guards/auth.guard';
 // Modules
 import { MaterialModule } from '@angular/material';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastModule } from 'ng2-toastr/ng2-toastr';
 
+
 const appRoutes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'app', component: PostloginComponent, canActivate: [AuthGuard],
+    children: [
+      { path: '', redirectTo: '', pathMatch: 'full' },
+      { path: 'switch', component: SwitchComponent },
+      { path: 'map', component: MapComponent },
+    ]
+  },
   { path: '',   redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: LoginComponent }
 ];
@@ -31,7 +45,10 @@ const appRoutes: Routes = [
     RegisterComponent,
     RegisterFormComponent,
     LoginComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    PostloginComponent,
+    SwitchComponent,
+    MapComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
@@ -45,6 +62,7 @@ const appRoutes: Routes = [
   ],
   providers: [
     UserService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
